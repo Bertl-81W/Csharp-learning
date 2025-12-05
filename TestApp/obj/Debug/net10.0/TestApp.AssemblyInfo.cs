f968c1cf2d7dba0b15ee5decab17c67d0792f750
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2025-12-05 12:40:03 -0700

    create initial C# .NET console project (Hello World)

--- TestApp/obj/Debug/net10.0/TestApp.AssemblyInfo.cs
+++ TestApp/obj/Debug/net10.0/TestApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe23f5026aba3875a5555865f8273c8e3711e96e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fee509865967f0d1a7ba06b5c221b80dc9a05f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
