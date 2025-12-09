2db9a31618e39c89d5b2f884172b1f8aa8dedaef
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2025-12-09 08:59:00 -0700

    learn numbers, precision and doubles

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -16,7 +16,12 @@ int b = 6;
 int c = a + b;
 Console.WriteLine(c); */
 
-int a = 210000000;
+/*int a = 210000000;
 int b = 210000000;
 long c = checked(a + b);
-Console.WriteLine(c);
+Console.WriteLine(c); */
+
+decimal a = 42.1M; // explicit type
+decimal b = 38.2M;
+decimal c = a + b;
+Console.WriteLine($"The answer is {c}");
